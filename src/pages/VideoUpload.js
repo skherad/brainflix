961ea7f1d87688512b0710d434fae119d3b86930
Shahrzad Kheradmand <shahrzad.kheradmand@gmail.com>
2022-08-27 16:35:07 -0600

    upload page - desktop style

--- src/pages/VideoUpload.js
+++ src/pages/VideoUpload.js
@@ -36,34 +36,41 @@ const VideoUpload = () => {
     return (
       <section className='upload'>
         <h2 className='upload__title'>Upload Video</h2>
-        <h3 className='upload__label'>VIDEO THUMBNAIL</h3>
-        <div className='upload__thumbnail'></div>
+     
 
         {/* upload video form */}
         <form className='form' onSubmit={handleSubmit}>
-          <label htmlFor="videoTitle" className='form__label'>
-            TITLE YOUR VIDEO
-          </label>
-          <input 
-            name="videoTitle" 
-            id="videoTitle" 
-            placeholder='Add a title to your video'  
-            className='form__input'
-            onChange={handleTitle}
-            value={videoTitle}
-          ></input>
-          
-          <label htmlFor="videoDesc" className='form__label'>
-            ADD A VIDEO DESCRIPTION
-          </label>
-          <textarea 
-            name="videoDesc" 
-            id="videoDesc"
-            placeholder='Add a description to your video'  
-            className='form__textarea'
-            onChange={handleDesc}
-            value={videoDesc}
-          ></textarea>
+          <div  className='form__container-desktop'>
+            <div className='form__left-container'>
+              <h3 className='upload__label'>VIDEO THUMBNAIL</h3>
+              <div className='upload__thumbnail'></div>
+            </div>
+            <div className='form__right-container'>
+              <label htmlFor="videoTitle" className='form__label'>
+                TITLE YOUR VIDEO
+              </label>
+              <input 
+                name="videoTitle" 
+                id="videoTitle" 
+                placeholder='Add a title to your video'  
+                className='form__input'
+                onChange={handleTitle}
+                value={videoTitle}
+              ></input>
+              
+              <label htmlFor="videoDesc" className='form__label'>
+                ADD A VIDEO DESCRIPTION
+              </label>
+              <textarea 
+                name="videoDesc" 
+                id="videoDesc"
+                placeholder='Add a description to your video'  
+                className='form__textarea'
+                onChange={handleDesc}
+                value={videoDesc}
+              ></textarea>
+            </div>
+          </div>
           <div className='form__nonmobile-container'>
             <button className='form__button'>PUBLISH</button>
             <button 
